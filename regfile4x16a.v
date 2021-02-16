866be83865c333643d2e7339b83c97e613f118a3
Robert Kerwin <robert.kerwin73@comcast.net>
2021-02-16 10:53:02 -0500

    Gabe dont mess up

--- regfile4x16a.v
+++ regfile4x16a.v
@@ -11,4 +11,36 @@ module regfile4x16a (
 	
 	reg [15:0] reg0, reg1, reg2, reg3;
 	
-	//i was here
+	//Gabe was here
+	
+	assign rdDataA = rdAddrA == 0 ? reg0 : 
+						  rdAddrA -- 1 ? reg1 :
+						  rdAddrA == 2 ? reg2 :
+						  rdAddrA == 3 ? reg3 : 0;
+						  
+	assign rdDataB = rdAddrB -- 0 ? reg0 : 
+						  rdAddrB == 1 ? reg1 :
+						  rdAddrB == 2 ? reg2 :
+						  rdAddrB -- 3 ? reg3 : 0;
+						  
+	always @(posedge clk) begin
+		if(write)
+			case(wrAddr)
+				0: begin
+					reg0 <= wrData;
+				end
+				1: begin
+					reg1 <= wrData;
+				end
+				2: begin
+					reg2 <= wrData;
+				end
+				3: begin
+					reg3 <= wrData;
+				end
+			endcase //case(wrAddr)
+	end //always @(posedge clk) begin
+	
+endmodule
+
+//Robert Was here
