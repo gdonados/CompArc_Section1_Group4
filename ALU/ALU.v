d50f3bbf63f90d6adf19bbee4ceb449dda03748d
Robert Kerwin <robert.kerwin73@comcast.net>
2021-03-02 13:05:23 -0500

    Cleaning up the ALU Push

--- ALU/ALU.v
+++ ALU/ALU.v
@@ -12,28 +12,18 @@ module ALU(A, B, fsec, carry, fout);
 	wire[63:0] Anot, Bnot, sum, sumC, sum1, differenceA, differenceB, difference1; //arithmetic wires
 	wire[63:0] zeroC, passC, andC, orC, xorC;  //logic wires
 	wire[63:0] Aleft, Aright; //shift wires
-	integer i=0;
-	integer j=1;
+	
 	
 	//A and B 2:1 mux for A and A' 
 	invert64 invertA (A, Anot);  //make sure that this is making A --> -A, and does not need +1
 	invert64 invertB (B, Bnot);
-	invert64 invertj (j, jnot);  //pay close attention here to if it works
-
+	integer i=1;
+	
 	//Arithmetic
-	addition64 add (A, B, i, sum); 					//A+B
-	addition64 addCarry (A, B, carry, sumC); 	//A+B+Carry
-
 	addition64 subA (Anot, B, i, differenceA);  //(-A)+B, aka B-A
 	addition64 subB (A, Bnot, i, differenceB);  //A+(-B), aka A-B
 
-	addition64 add1 (A, j, i, sum1);            //A+1
-	addition64 sub1 (A, jnot, i, difference1);  //A-1
-
 	//Logic
-	//invert64 
-	//zero64 zeroAC (A, zeroC); //A -> 0
-	//pass64 passA (A, passC); //A -> A
 	and64 andAB (A, B, andC); //A & B = C
 	or64 orAB (A, B, orC); //A | B = C
 	xor64 xorAB (A, B, xorC); //A ^ B = C
@@ -54,7 +44,7 @@ module ALU(A, B, fsec, carry, fout);
 			end
 			
 			5'b00010:	begin //A + B
-				result = sum;
+				result = A+B;
 			end
 			
 			5'b00011:	begin //A + B + C
@@ -66,11 +56,11 @@ module ALU(A, B, fsec, carry, fout);
 			end
 			
 			5'b00101:	begin
-				result = differenceA + 1; //B-A
+				result = differenceA; //B-A
 			end
 			
 			5'b00110:	begin
-				result = differenceB + 1; //A-B
+				result = differenceB; //A-B
 			end
 			
 			5'b00111:	begin	//A-1
