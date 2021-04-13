f652ce419a53234afa28192c46863313f89ac2cb
Robert Kerwin <robert.kerwin73@comcast.net>
2021-04-13 12:30:55 -0400

    Program Counter Fully Functioning

--- TopLevel/ProgramCounter/Program_Counter.v
+++ TopLevel/ProgramCounter/Program_Counter.v
@@ -1,38 +1,47 @@
 `timescale 10ns/10ns
-module Program_Counter (clk, reset, PC_IN, PS, PC_OUT, PC4);
-	input clk, reset;
+module Program_Counter (clk, reset, PS, PC_IN, PC_OUT, PC4);
+	input clk, reset; 
 	input [1:0] PS;
 	input[63:0] PC_IN;
-	output [63:0] PC_OUT;
-	output [63:0] PC4;
+	output [63:0] PC_OUT; //to ROM
+	output [63:0] PC4; //to RAM or REG_File
 	
-	reg [63:0] PC;
-	
-	assign PC_OUT = PC_IN + 1;
-	assign PC4 = PC_IN + 4;
 
-    always @(*) begin
-		case (PS)
-		2'b00: begin
-			PC = PC;
-			end
-			
-		2'b01: begin
-			PC = PC + 1;
-			end
-			
-		2'b10: begin
-			PC = PC_IN;
-			end
-			
-		2'b11: begin
-			PC = PC + PC_IN;
-			end
-			
+	reg[63:0] PC;
+	
+	//always @(*) begin
+	//PC_OUT <= PC;
+	//end
+	
+	assign PC_OUT = PC;
+	
+	initial begin
+		PC <= 0;
+	end
+	
+	always @(posedge clk) begin		
+		if(reset == 1) PC <= 0;
+		else begin	
+			case (PS)
+			2'b00: begin
+				PC <= PC;//Stall PC
+				end
+				
+			2'b01: begin //increment PC
+				PC <= PC + 16'h1;
+				end
+			2'b10: begin //load input value to set PC to jump to a spot in the code
+				PC <= PC_IN; 
+				end
+			2'b11: begin //offset PC by input value
+				PC <= PC + PC_IN; 
+				end
+			default begin
+				PC <= 0;
+				end
 			endcase
-			
-			end
-			
-			endmodule
+		end
+	end
+endmodule
 			
 	
---
+++ TopLevel/ProgramCounter/Program_Counter_tb.v
@@ -0,0 +1,41 @@
+`timescale 10ns/10ns
+module Program_Counter_tb();
+	reg clk, rst;
+	reg [1:0] PS;
+	reg [63:0] PC_IN;
+	
+	wire [63:0] PC_OUT, PC4;
+	
+	Program_Counter dut (clk, rst, PS, PC_IN, PC_OUT, PC4);
+	
+	
+	initial begin
+		clk <= 1'b1;
+		rst <= 1'b0;
+		PS <= 2'b00;
+		PC_IN <= 64'b0;
+		
+		#10 PS <= 2'b01; //increment by 1 instruction
+	
+		#10
+		PS <= 2'b10;
+		PC_IN <= 16; //jump to instruction 4
+	
+		#10
+		PS <= 2'b10;
+		PC_IN <= 4; //jump back to instruction 1
+	
+		#10
+		PS <= 2'b11;
+		PC_IN <= 32; //jump forward 8 instructions
+		
+		#10 rst <= 1'b1;
+	
+		#20 $stop;
+	end
+	
+	always
+	#5 clk <= ~clk;
+	
+endmodule
+	
